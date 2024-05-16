d8ef1432ba4f81170cfa1402317dec6a55778f45
jzjwonderful <jzjwonderful@outlook.com>
2024-05-16 08:34:25 +0800

    init commit

---
+++ publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var pubSocket = new PublisherSocket())
+            {
+                Console.WriteLine("Publisher socket binding...");
+                pubSocket.Options.SendHighWatermark = 1000;
+                pubSocket.Bind("tcp://*:20045");
+                for (var i = 0; i < 100; i++)
+                {
+                    // 
+                    string msg = string.Format("{0}",Environment.TickCount);
+                    pubSocket.SendMoreFrame("TopicA").SendMoreFrame(msg).SendFrame(i.ToString());
+                    Thread.Sleep(10);
+                }
+
+                pubSocket.SendMoreFrame("TopicA").SendMoreFrame("End").SendFrame("End");
+                Thread.Sleep(2000);
+            }
+
+            
+        }
+    }
+}
---
+++ subscriber/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+namespace SubscriberA
+{
+    class Program
+    {
+        public static IList<string> allowableCommandLineArgs
+            = new[] { "TopicA", "TopicB", "All" };
+        static void Main(string[] args)
+        {
+            if (args.Length != 1 || !allowableCommandLineArgs.Contains(args[0]))
+            {
+                Console.WriteLine("Expected one argument, either " +
+                                  "'TopicA', 'TopicB' or 'All'");
+                Environment.Exit(-1);
+            }
+            string topic = args[0] == "All" ? "" : args[0];
+            Console.WriteLine("Subscriber started for Topic : {0}", topic);
+            using (var subSocket = new SubscriberSocket())
+            {
+                subSocket.Options.ReceiveHighWatermark = 1000;
+                subSocket.Connect("tcp://localhost:20045");
+                subSocket.Subscribe(topic);
+                Console.WriteLine("Subscriber socket connecting...");
+
+                Dictionary<long, long> latency_counter = new Dictionary<long, long>();
+                long expected_msg_index = 0;
+                long msg_count = 0;
+                while (true)
+                {
+                    msg_count++;
+                    string messageTopicReceived = subSocket.ReceiveFrameString();
+                    string messageReceived = subSocket.ReceiveFrameString();
+                    if (messageReceived == "End")
+                    {
+                        break;
+                    }
+                    long index = long.Parse(subSocket.ReceiveFrameString());
+                    var recv_time = Environment.TickCount;
+                    var send_time = long.Parse(messageReceived);
+                    var latency = recv_time - send_time;
+                    latency_counter[latency] = latency_counter.ContainsKey(latency) ? latency_counter[latency] + 1 : 1;
+                    if (index != expected_msg_index)
+                    {
+                        Console.WriteLine("Missed message{0}", expected_msg_index);
+                    }
+                    expected_msg_index = index + 1;
+
+                    Console.WriteLine("Received message: {0} {1} {2}", messageTopicReceived, messageReceived, index);
+                }
+                Console.WriteLine("Latency distribution:");
+                foreach (var latency in latency_counter)
+                {
+                    Console.WriteLine("Latency: {0}ms, Count: {1}", latency.Key, latency.Value);
+                }
+                Console.WriteLine("count of received messages: {0}", msg_count);
+                Console.WriteLine("Subscriber finished");
+            }
+        }
+    }
+}
